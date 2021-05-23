4a111c633e83bcc8d90de1205573c99e42adb8c1
tonumber <77855848+tonumber@users.noreply.github.com>
2021-05-23 16:18:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const axios = require('axios')
 
 class PSUNode {
-  constructor(key) {
-    console.log("Initialized.")
-    this.apikey = key
-  }
-  getkey() {
-  return(this.apikey)
-  }
-  async obfuscate(script,options) {
-    var req = await axios.post('',{
-  "script": script,
-  "key": this.apikey,
-  "options": options
-})
-    return req.data
-  }
+    constructor(key) {
+        this.apikey = key
+    }
+    getkey() {
+        return (this.apikey)
+    }
+    async obfuscate(script, options, scriptOnly) {
+        var req = await axios.post('https://api.psu.dev/obfuscate', {
+            "script": script,
+            "key": this.apikey,
+            "options": options
+        })
+        if (scriptOnly) {
+            return req.data.data
+        } else {
+            return req.data
+        }
+    }
 }
 
 module.exports = PSUNode;
