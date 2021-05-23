f6a855b2e5821beb23f1987b98b7e95591434985
tonumber <77855848+tonumber@users.noreply.github.com>
2021-05-23 16:18:58 -0400

    Create test.js

---
+++ examples/test.js
@@ -0,0 +1,17 @@
+const api = require('./index.js') // replace with psu-nodejs
+const wrapper = new api('APIKey Here')
+async function init() {
+    var a = await wrapper.obfuscate("print('e')", {
+            "DisableSuperOperators": false,
+            "MaximumSecurityEnabled": false,
+            "EncryptAllStrings": false,
+            "DisableAllMacros": false,
+            "EnhancedOutput": false,
+            "CompressedOutput": false,
+            "PremiumFormat": false,
+            "ByteCodeMode": "Default"
+        }, true // true for script only, remove if not (or set to false)
+    )
+    console.log(a)
+}
+init()
