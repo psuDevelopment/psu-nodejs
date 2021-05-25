14e5a495dd0db1b1591c3ac18a4cb494b725d15b
tonumber <77855848+tonumber@users.noreply.github.com>
2021-05-24 20:32:38 -0400

    Update test.js

--- examples/test.js
+++ examples/test.js
@@ -1,7 +1,7 @@
-const api = require('./index.js') // replace with psu-nodejs
-const wrapper = new api('APIKey Here')
-async function init() {
-    var a = await wrapper.obfuscate("print('e')", {
+const PSU = require('./index.js') // replace with psu-nodejs
+const wrapper = new PSU('APIKey Here');
+(async () => {
+  let a = await wrapper.obfuscate("print('e')", {
             "DisableSuperOperators": false,
             "MaximumSecurityEnabled": false,
             "EncryptAllStrings": false,
@@ -10,8 +10,23 @@ async function init() {
             "CompressedOutput": false,
             "PremiumFormat": false,
             "ByteCodeMode": "Default"
-        }, true // true for script only, remove if not (or set to false)
+        }, true // true for script return only, remove if you want the json object (or set to false)
+    ) console.log(a)
+  }
+  console.log(a)
+  
+  let b = await wrapper.obfuscate(["print('e')","print('xd')", "print(game.Players.LocalPlayer.name)"], {
+            "DisableSuperOperators": false,
+            "MaximumSecurityEnabled": false,
+            "EncryptAllStrings": false,
+            "DisableAllMacros": false,
+            "EnhancedOutput": false,
+            "CompressedOutput": false,
+            "PremiumFormat": false,
+            "ByteCodeMode": "Default"
+        }, true // true for script return only, remove if you want the json object (or set to false)
     )
-    console.log(a)
-}
-init()
+    // returns an array of promises
+    console.log(...b)
+  }
+})()
